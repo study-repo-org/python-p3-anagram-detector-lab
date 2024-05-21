1a7abd773f1370642297c66e7116c0594f36ba28
Mohamed Ahmed <ma07041705@gmail.com>
2024-05-21 08:18:11 +0300

    Anagram Detector Lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        matching_words = []
+        
+        for word in word_list:
+            if sorted(self.word) == sorted(word):
+                matching_words.append(word)
+
+        return matching_words
